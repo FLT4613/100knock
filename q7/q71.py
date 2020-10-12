6bccd1f1aed365169f6f5f357ca5c6071f8a6e66
FLT <ft4613st@gmail.com>
2020-10-13 08:58:00 +0900

    update

--- q7/q71.py
+++ q7/q71.py
@@ -7,7 +7,12 @@ import torch.nn.functional as F
 vec_x = torch.from_numpy(np.loadtxt('files/q7_train_x.txt'))
 table_y = torch.from_numpy(np.loadtxt('files/q7_train_y.txt'))
 
-w = torch.randn(vec_x.shape[1], 4)
-print(w[0].shape)
-print(vec_x[0].shape)
-print(F.softmax(vec_x[0] * w[1]))
+w = torch.randn(vec_x.shape[1], 4).double()
+
+print(vec_x[0])
+print(w.shape)
+
+# print(F.softmax(vec_x[0] * w.T, dim=1).shape)
+# print(F.softmax(vec_x[0] * w.T, dim=1).shape)
+print(F.softmax(torch.matmul(vec_x[0], w)))
+print(F.softmax(torch.matmul(vec_x[0:4], w)))
