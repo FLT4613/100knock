de5fa51b316da66f68585ad939b0758d10cdb289
FLT <ft4613st@gmail.com>
2020-10-14 09:58:23 +0900

    update

--- q7/q73_q74.py
+++ q7/q73_q74.py
@@ -1,29 +1,38 @@
+from typing import Sequence
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import pandas as pd
+import torch.optim as optim
+from tqdm import tqdm
 
-vec_x = torch.tensor(np.loadtxt('files/q7_train_x.txt'))
-vec_y = torch.from_numpy(np.loadtxt('files/q7_train_y.txt')).long()
-test_vec_x = torch.tensor(np.loadtxt('files/q7_test_x.txt'))
-test_vec_y = pd.DataFrame(np.loadtxt('files/q7_test_y.txt'))
 
-w = torch.randn(vec_x.shape[1], 4).double().requires_grad_(True)
-learning_rate = 0.1
-loss = nn.CrossEntropyLoss()
+train_x = torch.tensor(np.loadtxt('files/q7_train_x.txt')).float()
+train_y = torch.tensor(np.loadtxt('files/q7_train_y.txt')).long()
+test_x = torch.tensor(np.loadtxt('files/q7_test_x.txt')).float()
+test_y = torch.tensor(np.loadtxt('files/q7_test_y.txt')).long()
 
-for _ in range(1000):
-    y_pred = torch.matmul(vec_x, w)
-    L = loss(y_pred, vec_y)
-    L.backward()
-    with torch.no_grad():
-        w -= learning_rate * w.grad
-        w.grad.zero_()
+model = nn.Sequential(*[
+    torch.nn.Linear(300, 4)
+])
 
-# q74
-# mm == matmul
-test_y_pred = F.softmax(torch.mm(test_vec_x, w), dim=1)
-pred_labels = pd.DataFrame(test_y_pred).astype('float').apply(lambda x: x.idxmax(), axis=1)
-e = pd.concat([pred_labels, test_vec_y], axis=1)
-print(len(e[e.iloc[:, 0] == e.iloc[:, 1]]) / len(test_vec_y))
+op = optim.SGD(model.parameters(), lr=0.1)
+celoss = nn.CrossEntropyLoss()
+
+epoch = 1000
+losses = []
+for i in tqdm(range(epoch)):
+    predict = model(train_x)
+    loss = celoss(predict, train_y)
+
+    op.zero_grad()
+    loss.backward()
+    op.step()
+    if i % 10 == 0:
+        losses.append(loss.item())
+
+
+# print(losses)
+model.eval()
+with torch.no_grad():
+    predict = torch.max(model(test_x), 1)
+    print(torch.eq(test_y, predict.indices).sum().item() / len(test_y))
