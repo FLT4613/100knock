07befd34c0e31c9cb3a9b8576ce3b74352ce0adb
FLT <ft4613st@gmail.com>
2020-10-17 07:25:56 +0900

    update

--- q8/q81.py
+++ q8/q81.py
@@ -1,14 +1,13 @@
-from time import time
-
-import numpy as np
+import pickle
+import pandas as pd
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from matplotlib import pyplot as plt
-from torch.utils.data import DataLoader, TensorDataset
-from tqdm import tqdm
-import pickle
 import torch.nn.functional as F
+from torch import matmul, tanh
+
+# https://pytorch.org/docs/stable/generated/torch.set_printoptions.html#torch-set-printoptions
+# 指数表記を無効にする
+torch.set_printoptions(sci_mode=False)
 
 with open('files/q80_output.pickle', 'rb') as f:
     idlist = pickle.load(f)
@@ -28,47 +27,34 @@ d_w = 300
 d_h = 50
 L = 4
 
-w_hx = torch.randn(d_w, d_h)
+w_hx = torch.randn(d_h, d_w)
 w_hh = torch.randn(d_h, d_h)
 w_yh = torch.randn(L, d_h)
 
-b = torch.randn(d_h)
-b = torch.randn(d_h)
+b_h = torch.randn(d_h)
+b_y = torch.randn(L)
 
-
-def emb(vec):
-    return F.embedding(vec, w_hx)
+h_T = torch.tensor([])
 
 
 def rnn(x, h):
-    pass
+    return tanh(matmul(w_hx, x) + matmul(w_hh, h) + b_h)
 
 
-def y(h_T, b_y):
-    pass
+def y(h_T):
+    return F.softmax(matmul(w_yh, h_T.T), dim=0)
 
 
-title = "Marvel's Guardians of the Galaxy Test: Making Star-Lord Into an Actual Movie Star"
-title_ids = to_idlist(title)
-x = [
-    torch.tensor([1 if i == title_id else 0 for i in range(1, len(voc))])
-    for title_id in title_ids
-]
-print(emb(x[0]).shape)
+# preprocess
+train = pd.read_csv('files/train.txt', delimiter='\t', header=None).iloc[:, 0]
+id_list = sorted(train.apply(to_idlist).tolist(), key=lambda x: len(x), reverse=True)
+max_size = len(id_list[0])
+emb = nn.Embedding(len(voc), d_w, padding_idx=0)
+x = torch.tensor([(x + [0] * (max_size - len(x))) for x in id_list])[0]
 
-exit()
+# fitting
+h_t = torch.zeros(d_h)
+for x_t in x:
+    h_t = rnn(emb(x_t), h_t)
 
-print(b)
-hat_y1 = F.softmax()
-exit()
-train_ds = TensorDataset(train_x, train_y)
-train_dl = DataLoader(train_ds, batch_size=bs, shuffle=True)
-model = nn.Sequential(*[
-    torch.nn.Linear(300, 4)
-])
-op = optim.SGD(model.parameters(), lr=0.1)
-loss = nn.CrossEntropyLoss()
-epoch = 10
-model.train()
-
-start = time()
+print(y(h_t))
