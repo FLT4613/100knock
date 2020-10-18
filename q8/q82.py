78b6c3e9afebd2486623c8691b4f6999669cc1ec
FLT <ft4613st@gmail.com>
2020-10-19 08:10:53 +0900

    複数タイトルを処理できるようにした

--- q8/q82.py
+++ q8/q82.py
@@ -38,11 +38,11 @@ h_T = torch.tensor([])
 
 
 def rnn(x, h):
-    return tanh(matmul(w_hx, x) + matmul(w_hh, h) + b_h)
+    return tanh(matmul(w_hx, x.T) + matmul(w_hh, h))
 
 
 def y(h_T):
-    return F.softmax(matmul(w_yh, h_T.T), dim=0)
+    return F.softmax(matmul(w_yh, h_T), dim=0)
 
 
 # preprocess
@@ -50,11 +50,13 @@ train = pd.read_csv('files/train.txt', delimiter='\t', header=None).iloc[:, 0]
 id_list = sorted(train.apply(to_idlist).tolist(), key=lambda x: len(x), reverse=True)
 max_size = len(id_list[0])
 emb = nn.Embedding(len(voc), d_w, padding_idx=0)
-x = torch.tensor([(x + [0] * (max_size - len(x))) for x in id_list])[0]
+x = torch.tensor([(x + [0] * (max_size - len(x))) for x in id_list])
 
-# fitting
-h_t = torch.zeros(d_h)
-for x_t in x:
+# # fitting
+h_t = torch.zeros(d_h, len(train))
+
+for i in range(max_size):
+    x_t = x[:, i]
     h_t = rnn(emb(x_t), h_t)
 
-print(y(h_t))
+print(y(h_t).T)
