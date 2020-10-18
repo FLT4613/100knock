a49d1c51a854b7eedc20072ffaf6ad543026f957
FLT <ft4613st@gmail.com>
2020-10-19 08:15:06 +0900

    update

--- q8/q82.py
+++ q8/q82.py
@@ -1,13 +1,15 @@
+# https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial.html
+
 import pickle
 import pandas as pd
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch import matmul, tanh
-
+import torch.optim as optim
+from tqdm import tqdm
+from torch.utils.data import DataLoader, TensorDataset
 # https://pytorch.org/docs/stable/generated/torch.set_printoptions.html#torch-set-printoptions
 # 指数表記を無効にする
-torch.set_printoptions(sci_mode=False)
+# torch.set_printoptions(sci_mode=False)
 
 with open('files/q80_output.pickle', 'rb') as f:
     idlist = pickle.load(f)
@@ -23,40 +25,74 @@ def to_idlist(s):
     return [idlist.get(w, 0) for w in s]
 
 
+# preprocess
+def get_dataset(path):
+    mapping = ['b', 't', 'e', 'm']
+    table = pd.read_csv(path, delimiter='\t', header=None)
+    table.iloc[:, 0] = table.iloc[:, 0].apply(to_idlist)
+    table.iloc[:, 1] = table.iloc[:, 1].apply(lambda x: mapping.index(x))
+    max_size = table.iloc[:, 0].apply(lambda x: len(x)).max()
+    table.iloc[:, 0] = table.iloc[:, 0].apply(lambda x: [0] * (max_size - len(x)) + x)
+    x = torch.tensor(table.iloc[:, 0])
+    y = torch.tensor(table.iloc[:, 1])
+    return TensorDataset(x, y)
+
+
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Net, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(len(voc), input_size, padding_idx=0)
+        self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
+        self.linear = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input):
+        x = self.embedding(input)
+        _, h = self.rnn(x)
+        # 系列の最後の隠れ状態を使う
+        y = self.softmax(self.linear(h[0]))
+        return y
+
+
+train_dataset = get_dataset('files/train.txt')
+test_dataset = get_dataset('files/test.txt')
+
+batch_size = 10
 d_w = 300
 d_h = 50
 L = 4
 
-w_hx = torch.randn(d_h, d_w)
-w_hh = torch.randn(d_h, d_h)
-w_yh = torch.randn(L, d_h)
+rnn = Net(d_w, d_h, L)
+loss_function = nn.NLLLoss()
+op = optim.SGD(rnn.parameters(), lr=0.075)
+losses = []
+epoch = 50
 
-b_h = torch.randn(d_h)
-b_y = torch.randn(L)
+for e in range(epoch):
+    rnn.train()
+    dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    progress = tqdm(enumerate(dataloader), total=len(dataloader))
+    for count, (x_batch, y_batch) in progress:
+        predict = rnn(x_batch)
+        op.zero_grad()
+        loss = loss_function(predict, y_batch)
+        loss.backward()
+        op.step()
+        # with torch.no_grad():
+        #     losses.append(loss.item())
 
-h_T = torch.tensor([])
-
-
-def rnn(x, h):
-    return tanh(matmul(w_hx, x.T) + matmul(w_hh, h))
-
-
-def y(h_T):
-    return F.softmax(matmul(w_yh, h_T), dim=0)
-
-
-# preprocess
-train = pd.read_csv('files/train.txt', delimiter='\t', header=None).iloc[:, 0]
-id_list = sorted(train.apply(to_idlist).tolist(), key=lambda x: len(x), reverse=True)
-max_size = len(id_list[0])
-emb = nn.Embedding(len(voc), d_w, padding_idx=0)
-x = torch.tensor([(x + [0] * (max_size - len(x))) for x in id_list])
-
-# # fitting
-h_t = torch.zeros(d_h, len(train))
-
-for i in range(max_size):
-    x_t = x[:, i]
-    h_t = rnn(emb(x_t), h_t)
-
-print(y(h_t).T)
+    rnn.eval()
+    with torch.no_grad():
+        train_x, train_y = train_dataset.tensors
+        train_result = rnn(train_x)
+        train_predict = torch.max(train_result, 1)
+        train_loss = loss_function(train_result, train_y)
+        test_x, test_y = test_dataset.tensors
+        test_result = rnn(test_x)
+        test_predict = torch.max(test_result, 1)
+        test_loss = loss_function(test_result, test_y)
+        print(f'Train Loss\t: {train_loss}')
+        print(f'Train Accuracy\t: {torch.eq(train_y, train_predict.indices).sum().item() / len(train_y)}')
+        print(f'Test Loss\t: {test_loss}')
+        print(f'Test Accuracy\t: {torch.eq(test_y, test_predict.indices).sum().item() / len(test_y)}')
