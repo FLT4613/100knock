9179b5b20f74df0a88104a4799c1a4fb48b281b6
Furuki <furuki.takahiro@jp.fujitsu.com>
2020-10-23 18:48:23 +0900

    update

--- chapter10_machine_translation/q91.py
+++ chapter10_machine_translation/q91.py
@@ -1,16 +1,10 @@
 # https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html
 # https://huggingface.co/Helsinki-NLP/opus-mt-ja-en
-from transformers import BertForSequenceClassification, BertTokenizer
-import pandas as pd
+import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 from tqdm import tqdm
-import spacy
-import torch.nn.functional as F
-
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import (AutoModelForSeq2SeqLM, AutoTokenizer)
 
 # q83
 if torch.cuda.is_available():
@@ -29,43 +23,52 @@ class Trainer:
             self.model.train()
             dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
             for x_batch, y_batch in tqdm(dataloader, total=len(dataloader)):
-                cfg = tokenizer.prepare_seq2seq_batch(x_batch, y_batch)
-                # print(cfg)
-                loss = self.model(**cfg)
-                print(loss)
-                exit()
+                cfg = tokenizer.prepare_seq2seq_batch(x_batch, y_batch).to(device)
+                loss, _, _ = self.model(**cfg)
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
             self.model.eval()
             with torch.no_grad():
-                train_x, train_y = train_dataset.tensors
-                cfg = tokenizer.prepare_seq2seq_batch(train_x, train_y)
-                train_loss, train_logit = self.model(**cfg)
-                train_predict = torch.max(train_logit, 1)
+                train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+                train_losses = []
+                train_accuracy = []
+                for x_batch, y_batch in train_dataloader:
+                    train_cfg = tokenizer.prepare_seq2seq_batch(x_batch, y_batch).to(device)
+                    train_loss, train_logit, _ = self.model(**train_cfg)
+                    train_losses.append(train_loss.item())
+                    train_predict = torch.max(train_logit, 2)
+                    for label, predict in zip(train_cfg.labels, train_predict.indices):
+                        train_accuracy.append(torch.eq(label, predict).sum().item() / predict.shape[0])
 
-                tune_x, tune_y = tune_dataset.tensors
-                cfg = tokenizer.prepare_seq2seq_batch(tune_x, tune_y)
-                tune_loss, tune_logit = self.model(**cfg)
-                tune_predict = torch.max(tune_logit, 1)
+                tune_dataloader = DataLoader(tune_dataset, batch_size=batch_size)
+                tune_losses = []
+                tune_accuracy = []
+                for x_batch, y_batch in tune_dataloader:
+                    tune_cfg = tokenizer.prepare_seq2seq_batch(x_batch, y_batch).to(device)
+                    tune_loss, tune_logit, _ = self.model(**tune_cfg)
+                    tune_losses.append(tune_loss.item())
+                    tune_predict = torch.max(tune_logit, 2)
+                    for label, predict in zip(tune_cfg.labels, tune_predict.indices):
+                        tune_accuracy.append(torch.eq(label, predict).sum().item() / predict.shape[0])
 
-                print(f'Train Loss\t: {train_loss}')
-                print(f'Train Accuracy\t: {torch.eq(train_y, train_predict.indices).sum().item() / len(train_y)}')
-                print(f'Tune Loss\t: {tune_loss}')
-                print(f'Tune Accuracy\t: {torch.eq(tune_y, tune_predict.indices).sum().item() / len(tune_y)}')
+                print(f'Train Loss\t: {np.array(train_losses).mean()}')
+                print(f'Train Accuracy\t: {np.average(train_accuracy)}')
+                print(f'Tune Loss\t: {np.array(tune_losses).mean()}')
+                print(f'Tune Accuracy\t: {np.average(tune_accuracy)}')
 
 
 if __name__ == "__main__":
-    batch_size = 10
+    batch_size = 16
     epoch = 10
 
     src = None
     target = None
 
     def load(type_name):
-        with open(f'files/kftt-data-1.0/data/tok/kyoto-{type_name}.ja') as f:
-            with open(f'files/kftt-data-1.0/data/tok/kyoto-{type_name}.en') as g:
+        with open(f'files/kftt-data-1.0/data/orig/kyoto-{type_name}.ja', encoding='utf-8') as f:
+            with open(f'files/kftt-data-1.0/data/orig/kyoto-{type_name}.en', encoding='utf-8') as g:
                 return list(zip(f.readlines(), g.readlines()))
 
     train_dataset = load('train')
@@ -74,7 +77,7 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ja-en")
     model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ja-en")
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
     trainer = Trainer(
         model=model,
         optimizer=optimizer
